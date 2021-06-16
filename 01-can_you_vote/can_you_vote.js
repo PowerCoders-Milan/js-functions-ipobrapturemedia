b52febc3fe20adc3e62882de6032522e76e2d280
ipobrapturemedia <ipobrapturemedia@gmail.com.com>
2021-06-16 11:48:53 +0200

    can_you_vote.js 1 don

--- 01-can_you_vote/can_you_vote.js
+++ 01-can_you_vote/can_you_vote.js
@@ -1,15 +1,17 @@
 // define a function that will check if I can vote or not
 const canIVote = age => {
     // define a variable legalAge
-
+legalAge = 18
+age = Math.ceil(Math.random()*100)
     // create if else statement that console.log() if you can vote or not
-    if (condition) {
-        
+    if (age=>legalAge) {
+        return (console.log('${age}"you are allowed to vote"'));
     } else {
-        
+        return (console.log('${age}"not allowed to vote vote"'));
     }
 }
 
 // define a variable for your age
 
-// call the function and see if you can vote
+// call the function and see if you can vote 
+canIVote(18);
